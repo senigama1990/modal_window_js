82f0cd4d5777fde701de8b933cca61ff616920f8
senigama1990 <senigama1990@mail.ru>
2022-08-04 23:33:07 +0500

    finished modal window js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 'use strict';
 
-const modalWindow = document.querySelector(".modal-window")
-const overlay = document.querySelector(".overlay")
-const btnCloseModalWindow = document.querySelector(".close-modal-window")
-const btnsShowModalWindow = document.querySelectorAll(".show-modal-window")
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
 
-for (let i = 0; i < btnsShowModalWindow.length; i++){
-  console.log(btnsShowModalWindow[i].textContent);
+function showModalWindow() {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
+
+function closeModalWindow() {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].onclick = showModalWindow;
+}
+
+btnCloseModalWindow.onclick = closeModalWindow;
+overlay.onclick = closeModalWindow;
+
+document.onkeydown = e => {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
